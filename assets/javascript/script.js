789d816e9c35b8b341c2a2f7aad0d6fd057e83f0
Gianni <gianni.giubel@gmail.com>
2023-11-29 19:47:51 -0300

    terminado as validações do campo nome, sobrenome, email e mensagem

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,14 +4,55 @@ const email = document.querySelector('#email')
 const message = document.querySelector('#message')
 const send_form = document.querySelector('#send-form')  
 
+function valid_name(name) {
+    const regex_name = new RegExp(
+        /^[a-zA-Z]+(\s[a-zA-Z]+)?$/
+    )
+
+    if(name.value === '') {
+        name.setAttribute("placeholder" , "* Required field")
+        name.classList.add("highlight")        
+    } else {
+        name.classList.remove("highlight")
+        if(regex_name.test(name.value)) {
+            return true
+        } else {
+            alert("Preencha seu nome (segundo nome(opcional)) sem utilizar números ou caracteres especiais!")
+        }
+    }
+
+    return false
+
+}
+
+function valid_last_name(lastName) {
+    const regex_last_name = new RegExp(
+        /^[a-zA-Z]+(\s[a-zA-Z]+)?$/
+    )
+
+    if(lastName.value === '') {
+        lastName.setAttribute("placeholder" , "* Required field")  
+        lastName.classList.add("highlight")       
+    } else {
+        if(regex_last_name.test(lastName.value)) {
+            return true
+        } else {
+            alert("Preencha seu sobrenome (segundo sobrenome(opcional)) sem utilizar números ou caracteres especiais!")
+        }
+    }
+
+    return false
+
+}
+
 function valid_email(email) {    
     const regex_email = new RegExp (
-        /^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]+(\.[a-zA-Z]+)?$/
+        /^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}?$/
     )
 
     if(email.value === '') {
         email.setAttribute("placeholder" , "* Required field")
-        return
+        email.classList.add("highlight")         
     } else {    
         if(regex_email.test(email.value)) {
             return true
@@ -24,37 +65,43 @@ function valid_email(email) {
          
 }
 
-function valid_name(name) {
-    const regex_name = new RegExp(
-        /^[a-zA-Z]+([a-zA-Z])/
+function valid_message(message) {
+    const regex_message = new RegExp(
+        /^((?!\b(pênis|penis|desgraçado|desgracado|cacete|vagina|vágina)\b).)+?$/i
     )
 
-    if(name.value === '') {
-        name.setAttribute("placeholder" , "* Required field")
+    if(message.value !== '') {
+        if(regex_message.test(message.value)) {
+            return true
+        } else {
+            alert("Sua mensagem não pode conter palavras proibidas!")
+            return false
+        }
+    }
+}
+
+function valid_form(){
+
+    if(!valid_name(first_name)) {
+        return
+    }
+
+    if(!valid_last_name(last_name)) {
+        return
+    }
+
+    if(!valid_email(email)) {        
         return
     } 
 
-}
-
-function valid_last_name(lastName) {
-
-    if(lastName.value === '') {
-        lastName.setAttribute("placeholder" , "* Required field")
+    if(!valid_message(message)) {
         return
     }
 
 }
 
-send_form.addEventListener('click', (event) => {  
+send_form.addEventListener('click', (event) => {        
     
-    event.preventDefault()   
-    
-    valid_name(first_name)
-
-    valid_last_name(last_name)
-
-    if(!valid_email(email)) {        
-        return
-    }      
+    valid_form() 
 
 }) 
